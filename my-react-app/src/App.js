3a3cdc6c3431c184b9a249c4069614c0e6934e78
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-08 17:24:53 +0700

    Our Starting Setup

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import SimpleInput from "./components/SimpleInput";
+import BasicForm from "./components/BasicForm";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="app">
+        <SimpleInput />
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/SimpleInput.js
@@ -0,0 +1,17 @@
+import react from "react";
+
+const SimpleInput = (props) => {
+  return (
+    <form>
+      <div className='form-control'>
+        <label htmlFor='name'>Your Name</label>
+        <input type='text' id='name' />
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
